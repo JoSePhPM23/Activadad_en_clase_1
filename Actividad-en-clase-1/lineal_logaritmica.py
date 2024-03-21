562938455424ea1c9aeee43236ae85b70df023ea
JoSePhPM23 <128115332+JoSePhPM23@users.noreply.github.com>
2024-03-20 21:47:07 -0600

    Update lineal_logaritmica.py

--- Actividad-en-clase-1/lineal_logaritmica.py
+++ Actividad-en-clase-1/lineal_logaritmica.py
@@ -1,13 +1,15 @@
 import time
+import random
 
-#Algoritmo de ordenamiento Quicksort de complejidad lineal logaritmico 
+# Algoritmo de ordenamiento Quicksort de complejidad lineal logarítmica
 def quicksort(array):
     if len(array) <= 1:
         return array
     else:
-        pivot = array[0]
-        menores = [x for x in array[1:] if x <= pivot]
-        mayores = [x for x in array[1:] if x > pivot]
+        pivot_index = random.randint(0, len(array) - 1)
+        pivot = array[pivot_index]
+        menores = [x for i, x in enumerate(array) if x <= pivot and i != pivot_index]
+        mayores = [x for i, x in enumerate(array) if x > pivot and i != pivot_index]
         return quicksort(menores) + [pivot] + quicksort(mayores)
 
 # Función para medir el tiempo de ejecución
@@ -18,13 +20,15 @@ def medir_tiempo(funcion, *args):
         funcion(*args)
     fin = time.time_ns()
     # Tiempo promedio en nanosegundos
-    tiempo_promedio = (fin - inicio) / 10 
+    tiempo_promedio = (fin - inicio) / 10
+    return tiempo_promedio
 
 # Tamaños de arrays
-tamanos = [10, 100, 1000, 10000, 100000]
+tamanos = [10, 100, 1000, 10000]
 
 for tamano in tamanos:
     # Array invertido para el peor caso
     mi_array = list(range(tamano, 0, -1))   
     tiempo = medir_tiempo(quicksort, mi_array)
     print(f"Tamaño del array: {tamano}, Tiempo promedio de ejecución: {tiempo} nanosegundos")
+
